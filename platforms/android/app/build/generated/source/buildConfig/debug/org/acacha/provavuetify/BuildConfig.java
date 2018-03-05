70610b0623bedf09a50d545bd83338dfd7c6e930
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-03-05 16:44:03 +0100

    Initial version

---
+++ platforms/android/app/build/generated/source/buildConfig/debug/org/acacha/provavuetify/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.acacha.provavuetify;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.acacha.provavuetify";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
